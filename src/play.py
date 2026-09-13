c917c101a9b30c45a0478b92896626e661911254
williamwu277 <williamwu277@gmail.com>
2026-09-13 18:36:34 -0400

    Make CLI input more durable

--- src/play.py
+++ src/play.py
@@ -14,11 +14,17 @@ strategy_registry = [
 ]
 
 
-def play_game():
-    bot_count = int(input("Number of bots [1, 3]: "))
+def durable_input(prompt: str, start: int, end: int) -> int:
+    while True:
+        result = int(input(prompt))
+        if start <= result <= end:
+            return result
+        else:
+            print("Invalid input")
 
-    if bot_count > MAX_BOTS or bot_count <= 0:
-        raise ValueError("Invalid scenario")
+
+def play_game():
+    bot_count = durable_input("Number of bots [1, 3]: ", 1, 3)
 
     print(f"We currently have a registry of {len(strategy_registry)} bot types:")
     for bot_id, bot_type in enumerate(strategy_registry, start=1):
@@ -28,21 +34,16 @@ def play_game():
     players = [create_player(Strategy.HUMAN, "Human Player")]
 
     for i in range(1, bot_count + 1):
-        bot_id = int(input(f"Bot {i}: "))
-
-        if bot_id < 1 or bot_id > len(strategy_registry):
-            raise ValueError("Invalid scenario")
-
+        bot_id = durable_input(f"Bot {i}: ", 1, len(strategy_registry))
         bot_type = strategy_registry[bot_id - 1]
         players.append(create_player(bot_type, f"{bot_type!s}-{i}"))
 
     shuffle(players)
 
     max_card_count = FULL_DECK_SIZE // len(players)
-    card_count = int(input(f"Number of cards per player [1, {max_card_count}]: "))
-
-    if not 1 <= card_count <= max_card_count:
-        raise ValueError(f"Cards per player must be between 1 and {max_card_count}")
+    card_count = durable_input(
+        f"Number of cards per player [1, {max_card_count}]: ", 1, max_card_count
+    )
 
     game = President(players=players, cards_per_player=card_count)
 
